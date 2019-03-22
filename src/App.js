3ced0c7fecc17f2349f96c22408e1ebd9470583d
Mark Dewey <mark.t.dewey@gmail.com>
2019-03-22 11:38:40 -0400

    added basic map

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+
+import ReactMapGL from "react-map-gl"
+import "./App.css"
+
+const TOKEN =
+	"pk.eyJ1IjoiZGV3c2VwaCIsImEiOiJjanRrNzQwYTYwMHdjM3lwNnh2am05ejc0In0._LCOTuYCw-eRKDvUut2TxQ"
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	state = {
+		viewport: {
+			latitude: 37.7577,
+			longitude: -122.4376,
+			zoom: 8
+		}
+	}
+
+	render() {
+		return (
+			<>
+				<ReactMapGL
+					{...this.state.viewport}
+					mapboxApiAccessToken={TOKEN}
+					className="map"
+					width="100%"
+					height="100%"
+					onViewportChange={viewport => this.setState({ viewport })}
+				/>
+			</>
+		)
+	}
 }
 
-export default App;
+export default App
