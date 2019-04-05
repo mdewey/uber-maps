4de1008a9cf6a3d9ff37a54961a7bd8dd0e2da9b
Mark Dewey <mark.t.dewey@gmail.com>
2019-04-04 22:54:33 -0400

    done for now

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import React, { Component } from "react"
-
-import ReactMapGL from "react-map-gl"
-import "./App.css"
+import React, { Component } from "react";
+import CityPin from "./Pin";
+import ReactMapGL, {
+	Marker,
+	Popup,
+	NavigationControl,
+	FullscreenControl
+} from "react-map-gl";
+import "./App.css";
 
 const TOKEN =
-	"pk.eyJ1IjoiZGV3c2VwaCIsImEiOiJjanRrNzQwYTYwMHdjM3lwNnh2am05ejc0In0._LCOTuYCw-eRKDvUut2TxQ"
+	"pk.eyJ1IjoiZGV3c2VwaCIsImEiOiJjanRrNzQwYTYwMHdjM3lwNnh2am05ejc0In0._LCOTuYCw-eRKDvUut2TxQ";
 
 class App extends Component {
-	state = {
-		viewport: {
-			latitude: 37.7577,
-			longitude: -122.4376,
-			zoom: 8
-		}
+	constructor(props) {
+		super(props);
+		this.state = {
+			viewport: {
+				latitude: 27.950575,
+				longitude: -82.457176,
+				zoom: 2
+			},
+			pins: [this.getRandomLocation()]
+		};
 	}
+
+	getRandomLocation = () => {
+		const lat = 27.950575 + Math.random() - Math.random() * 10; //getRandomLatInFlorida()
+		const lng = -82.457176 - Math.random() + Math.random() * 10;
+
+		return {
+			lat,
+			lng
+		};
+	};
+
+	addPins = num => {
+		console.log("adding" + num + " pins");
+		const newPins = [...Array(num).keys()].map(this.getRandomLocation);
+		console.log(newPins);
+		this.setState(
+			{
+				pins: this.state.pins.concat(newPins)
+			},
+			() => {
+				console.log(this.state.pins.length);
+			}
+		);
+	};
+
+	renderPin = (pin, index) => {
+		return (
+			<Marker key={`marker-${index}`} longitude={pin.lng} latitude={pin.lat}>
+				<CityPin size={20} />
+			</Marker>
+		);
+	};
 
 	render() {
 		return (
 			<>
+				<div className="header">
+					<button onClick={() => this.addPins(1)}>add pin</button>
+					<button onClick={() => this.addPins(100)}>add 100 pin</button>
+					<button onClick={() => this.addPins(1000)}>add 1000 pin</button>
+					<button onClick={() => this.addPins(10000)}>add 10000 pin</button>
+				</div>
 				<ReactMapGL
 					{...this.state.viewport}
 					mapboxApiAccessToken={TOKEN}
@@ -25,10 +72,13 @@ class App extends Component {
 					width="100%"
 					height="100%"
 					onViewportChange={viewport => this.setState({ viewport })}
-				/>
+				>
+					{this.state.pins.map(this.renderPin)}
+					{/*  */}
+				</ReactMapGL>
 			</>
-		)
+		);
 	}
 }
 
-export default App
+export default App;
